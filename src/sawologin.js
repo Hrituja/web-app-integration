2c3174e8c9ef1e4f20068ebcea14791657883720
Hrituja <hrkhatavkar@gmail.com>
2020-11-06 16:40:55 +0530

    first commit

--- src/sawologin.js
+++ src/sawologin.js
@@ -7,7 +7,7 @@ class SawoLogin extends React.Component {
         const config = {
             containerID: 'sawo-container',
             identifierType: 'email',
-            apiKey: '92cac627-d146-4cff-a161-454133893df7',
+            apiKey: '3f1c4b86-29b6-4170-b91f-9729d17d4abe',
           }
           const sawo = new Sawo(config)
           sawo.showForm()
